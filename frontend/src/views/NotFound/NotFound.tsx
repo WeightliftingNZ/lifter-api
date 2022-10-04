a4fad3f48e9430a8e625efb6d78c5f33846e7582
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-10-05 08:47:06 +1300

    fix: adding 404 page and stats to athletedetail

---
+++ frontend/src/views/NotFound/NotFound.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { Box } from "@mui/material";
+import React from "react";
+import Title from "../../components/Title";
+
+const NotFound: React.FC = () => {
+  return (
+    <Box>
+      <Title>Not Found</Title>
+    </Box>
+  );
+};
+
+export default NotFound;
