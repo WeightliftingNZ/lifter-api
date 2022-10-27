1bc6a919ef6533b28876e246d72a4cd83fff0598
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-10-28 10:14:35 +1300

    refactor: app file

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,10 @@
+"""App configuration."""
+
 from django.apps import AppConfig
 
 
 class UserConfig(AppConfig):
+    """User configuration for this application."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
